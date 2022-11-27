32f4cc2928b5d85c91a4b992d540297e03838df9
Danilo Andrade <andrade20danilo@gmail.com>
2022-11-26 22:36:17 -0300

    terminamos a integracao com o menu incial com o tabuleiro

---
+++ StartMenu.py
@@ -0,0 +1,75 @@
+# importação de módulos do pyglet
+import pyglet
+import pyglet.window.key
+
+width = 512 # largura da janela
+height = 512 # altura da janela
+title = "New Chess Brazil" # título da janela
+
+
+class StartMenu():
+    def __init__(self,width, height, title):
+        
+        self.width = width 
+        self.height = height
+        self.title = title
+        self.text1 = "DESAFIE SEU AMIGO NUMA" # texto na janela
+        self.text2 = "PARTIDA DE XADREZ" # texto na janela
+        self.New = "New" # texto na janelaself.
+        self.Chess = "Chess" # texto na janela
+        self.Brazil = "Brazil" # texto na janela
+        self.batch = pyglet.graphics.Batch() 
+        self.wallpaper = pyglet.image.load('public/capa.png')
+        self.imagebutton = pyglet.image.load('public/start.png')
+        self.sprite1 = pyglet.sprite.Sprite(self.wallpaper, x = 0, y = 0)
+        self.sprite2 = pyglet.sprite.Sprite(self.imagebutton, x = 200, y = 80)
+        self.label1 = pyglet.text.Label(self.text1, # criação de um rótulo com as devidas formatações
+                                font_name='Comic Sans MS', # fonte do texto
+                                font_size=15, # tamanho do texto
+                                x=self.width//2, y=self.height - 30, # posição do texto na janela
+                                anchor_x='center', anchor_y='center') # centralização do texto
+        self.label2 = pyglet.text.Label(self.text2,
+                                font_name='Comic Sans MS',
+                                font_size=15,
+                                x=self.width//2, y=self.height - 55,
+                                anchor_x='center', anchor_y='center')
+        self.labelN = pyglet.text.Label(self.New,
+                                font_name='Comic Sans MS',
+                                font_size=40,
+                                x=self.width - 325, y=self.height - 150,
+                                anchor_x='center', anchor_y='center')
+        self.labelN.color = (96, 169, 23, 255)
+        self.labelC = pyglet.text.Label(self.Chess,
+                                font_name='Comic Sans MS',
+                                font_size=40,
+                                x=self.width - 205, y=self.height - 150,
+                                anchor_x='center', anchor_y='center')
+        self.labelC.color = (227, 200, 0, 255)
+        self.labelB = pyglet.text.Label(self.Brazil,
+                                font_name='Comic Sans MS',
+                                font_size=40,
+                                x=self.width//2, y=self.height - 190,
+                                anchor_x='center', anchor_y='center')
+        self.labelB.color = (27, 161, 226, 255)
+
+    
+    def on_draw(self):
+        
+        self.sprite1.draw() # desenha sprite1 na janela
+        self.sprite2.draw() # desenha sprite2 na janela
+        self.label1.draw() # desenha label1 na janela
+        self.label2.draw() # desenha label2 na janela
+        self.labelN.draw() # desenha labelN na janela
+        self.labelC.draw() # desenha labelC na janela
+        self.labelB.draw() # desenha labelB na janela
+
+    def on_mouse_release(self, x, y, button, modifiers):
+        if self.startedButtonClicked(x,y):
+            return "game"
+        else:
+            return "start"
+
+    def startedButtonClicked(self,x,y):
+
+        return 200 <= x <= 312 and   80 <= y <= 131
+        
--- classes/MyWindow.py
+++ classes/MyWindow.py
@@ -1,9 +1,9 @@
 import pyglet
-
+from StartMenu import StartMenu
 
 class MyWindow(pyglet.window.Window):
 
-    def __init__(self, width, height, board, running, gamestate):
+    def __init__(self, width, height, board, running, gamestate,window = "start"):
         super().__init__(width, height, caption="Chess")
         self.board = board
         self.running = running
@@ -14,6 +14,8 @@ class MyWindow(pyglet.window.Window):
         self.click_y = 0
         self.delta_x = 0
         self.delta_y = 0
+        self.window = window
+        self.startMenu = StartMenu(width, height, "New Chess Brazil")
 
     def boardSquare(self, i, j):
         return self.board.board[i][j]
@@ -22,19 +24,37 @@ class MyWindow(pyglet.window.Window):
         return ((x - self.click_x)**2 + (y - self.click_y)**2) > value**2
 
     def on_draw(self):
+        self.clear()
+        if self.window == "game":
+            self.on_draw_game_menu()
+        elif self.window == "start":
+            self.startMenu.on_draw()
+
+    
+
+    def on_draw_game_menu(self):
         if self.running:
-            self.clear()
+            
             self.batch.draw()  # desenhar batches devido ao alto número de shapes
 
             for line in self.board.board:  # desenhar as peças normalmente
                 for square in line:
                     square.drawPiece()
-
+                
     def on_mouse_press(self, x, y, button, modifiers):
+        if self.window == "game":
+            self.on_mouse_press_game_window(x, y, button, modifiers)
+
+    def on_mouse_press_game_window(self,x, y, button, modifiers):
         self.click_x = x
         self.click_y = y
-
+    
     def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
+        if self.window == "game":
+            self.on_mouse_drag_game_window(x, y, dx, dy, buttons, modifiers)
+
+
+    def on_mouse_drag_game_window(self, x, y, dx, dy, buttons, modifiers):
         if pyglet.window.mouse.LEFT:
 
             if self.drag_circle(x, y, 5) and self.drag == 0:
@@ -52,7 +72,15 @@ class MyWindow(pyglet.window.Window):
                 i, j = self.board.squareClick(self.click_x, self.click_y)
                 self.boardSquare(i, j).changeImageCoord(x - self.delta_x, y - self.delta_y)
 
+
     def on_mouse_release(self, x, y, button, modifiers):
+        if self.window == "game":
+            self.on_mouse_release_game_window(x, y, button, modifiers)
+        elif self.window == "start":
+            self.window = self.startMenu.on_mouse_release(x, y, button, modifiers)
+
+
+    def on_mouse_release_game_window(self, x, y, button, modifiers):
         if pyglet.window.mouse.LEFT:
             if self.drag == 0 and type(self.gs.clicked) == int:
                 self.gs.clicked = self.board.pieceClick(x, y, self.gs)
